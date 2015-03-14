78e25f4af6c387f1e0b14552d57b8c16aa1496dc
Daniel Myasnikov <daniel.myasnikov@redant.com.au>
2015-03-15 10:33:34 +1100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 4.2'
+gem 'mysql2'
+gem 'haml'
+gem 'comfortable_mexican_sofa'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WarehouseCms::Application.config.secret_token = '4ecb347060fb11399c563bc5af881f7d894d7ab6ef1d526b48d50129aaf2b3bb4530bd1003c6c8c4f7ad93299dbf773f094adba5a4b98e090af7e071f697ef2c'

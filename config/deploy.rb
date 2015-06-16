07d89c5eecea6f01a224c64a46b932a4117b7300
Daniel Myasnikov <daniel.g.myasnikov@gmail.com>
2015-06-16 20:46:57 +1000

    asset compilation for welcome

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,13 +18,13 @@ set :deploy_to, "/home/ubuntu/apps/#{fetch(:application)}"
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/unicorn.rb',
-#                                       'config/unicorn_init.sh')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/unicorn.rb',
+                                      'config/unicorn_init.sh')
 
 # # Default value for linked_dirs is []
 # # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
-#                                                'public/assets', 'public/system', 'vendor/bundle')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
+                                               'public/assets', 'public/system', 'vendor/bundle')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

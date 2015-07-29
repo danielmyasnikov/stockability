80cfc9b8536d9ee5be74cc9d50e0db94f93233c3
Daniel Myasnikov <daniel.g.myasnikov@gmail.com>
2015-07-30 08:13:07 +1000

    typo fix

--- db/migrate/20150726224803_change_fields.rb
+++ db/migrate/20150726224803_change_fields.rb
@@ -4,7 +4,7 @@ class ChangeFields < ActiveRecord::Migration
     # rename_column :stock_levels, :bin, :bin_id
     # change_column :stock_levels, :bin_id, :integer
     rename_column :tour_entries, :location, :location_id
-    TourEntry.all.each { |sl| sl.udpate_column(:bin, sl.bin.to_i); sl.save! }
+    TourEntry.all.each { |sl| sl.update_column(:bin, sl.bin.to_i); sl.save! }
     rename_column :tour_entries, :bin, :bin_id
     change_column :tour_entries, :bin_id, 'integer USING CAST(bin_id as integer)'
   end

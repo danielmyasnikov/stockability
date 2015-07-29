a99c6c7402f881a7a812779c0692360859610dec
Daniel Myasnikov <daniel.g.myasnikov@gmail.com>
2015-07-30 08:12:08 +1000

    migration fix

--- db/migrate/20150723230530_change_stock_levels_columns.rb
+++ db/migrate/20150723230530_change_stock_levels_columns.rb
@@ -2,7 +2,7 @@ class ChangeStockLevelsColumns < ActiveRecord::Migration
   def change
     add_column :stock_levels, :location_id, :integer
 
-    StockLevel.all.each { |sl| sl.bin = sl.bin.to_i; sl.save! }
+    StockLevel.all.each { |sl| sl.udpate_column(:bin, sl.bin.to_i); sl.save! }
     rename_column :stock_levels, :bin, :bin_id
     change_column :stock_levels, :bin_id, 'integer USING CAST(bin_id as integer)'
   end

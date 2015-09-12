a4d478f45641d99db0ece976a2a86c073dd02e1c
Daniel Myasnikov <daniel.g.myasnikov@gmail.com>
2015-09-12 14:22:03 +1000

    completed api tests

--- spec/support/uat_request_params_helper.rb
+++ spec/support/uat_request_params_helper.rb
@@ -1,5 +1,5 @@
 HOST        = 'http://uat.stockability.com.au/'
-
+LOCATION_CODE = 'NEWLOC123'
 ### TOKENS ###
 since = { since: '2015-09-11' }
 tokens_api = 'api/v1/tokens.json'
@@ -58,12 +58,39 @@ HTTParty.put(HOST + update_api, query: update_barcode.merge(auth))
 
 ### STOCK LEVELS ###
 stock_levels_api = 'api/v1/stock_levels.json'
+HTTParty.get(HOST + stock_levels_api, query: auth)
+HTTParty.get(HOST + stock_levels_api, query: auth.merge(since))
 
 invalid_barcode= { stock_level: { bin_code: 'BIN001', batch_code: 'BATCH01' } }
-create_barcode = { stock_level: { bin_code: 'BIN001', sku: FIRSTPRODUCTSKU, batch_code: 'BATCH01', location_code: 'NEWLOC123' } }
+create_barcode = { stock_level: { bin_code: 'BIN001', sku: FIRSTPRODUCTSKU, batch_code: 'BATCH01', location_code: LOCATION_CODE } }
 HTTParty.post(HOST + stock_levels_api, query: invalid_barcode.merge(auth))
 HTTParty.post(HOST + stock_levels_api, query: create_barcode.merge(auth))
 
 update_stock_levels_api = 'api/v1/stock_levels/32.json'
 update_stock_levels     = { stock_level: { bin_code: 'BIN002', batch_code: 'BATCH05' } }
 HTTParty.put(HOST + update_stock_levels_api, query: update_stock_levels.merge(auth))
+
+### TOUR ###
+tours_api = 'api/v1/tours.json'
+HTTParty.get(HOST + tours_api, query: auth)
+HTTParty.get(HOST + tours_api, query: auth.merge(since))
+
+create_tour = { tour: { name: 'MYAWESOMETOUR', active: true, started: '2015-01-01'} }
+HTTParty.post(HOST + tours_api, query: create_tour.merge(auth))
+
+update_tour_api = "api/v1/tours/#{id}.json"
+update_tour = { tour: { name: 'MYAWESOMETOUR', active: false, completed: '2015-01-02'} }
+HTTParty.put(HOST + update_tour_api, query: update_tour.merge(auth))
+
+### TOUR ENTRIES ###
+tour_entries_api = 'api/v1/tour_entries.json'
+HTTParty.get(HOST + tour_entries_api, query: auth)
+HTTParty.get(HOST + tour_entries_api, query: auth.merge(since))
+
+create_tour_entry = { tour_entry: { tour_id: id, sku: FIRSTPRODUCTSKU, batch_code: 'BATCH06', quantity: 55, location_code: LOCATION_CODE, bin_code: 'BIN002', barcode: 'BAR001' } }
+resp = HTTParty.post(HOST + tour_entries_api, query: create_tour_entry.merge(auth))
+id = resp.parsed_response['id']
+
+update_tour_entry_api = "api/v1/tour_entries/#{id}.json"
+update_tour_entry = { tour_entry: { tour_id: id, sku: FIRSTPRODUCTSKU, batch_code: 'BATCH06', quantity: 56, location_code: LOCATION_CODE, bin_code: 'BIN002', barcode: 'BAR001' } }
+HTTParty.put(HOST + update_tour_api, query: update_tour_entry.merge(auth))

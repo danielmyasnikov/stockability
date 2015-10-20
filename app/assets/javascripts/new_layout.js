fe93689e9b86fc7b0d5fdac5c4244a3b965cc69d
Daniel Myasnikov <daniel.g.myasnikov@gmail.com>
2015-10-20 19:29:54 +1100

    [72] templates integration

---
+++ app/assets/javascripts/new_layout.js
@@ -0,0 +1,6 @@
+//= require jquery-2.1.1.js
+//= require bootstrap.min.js
+//= require plugins/metisMenu/jquery.metisMenu.js
+//= require plugins/slimscroll/jquery.slimscroll.min.js
+//= require inspinia.js
+//= require plugins/pace/pace.min.js

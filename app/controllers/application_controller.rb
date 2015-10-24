03ee9ac2555d432f7548cef77ac0240f3816008c
Daniel Myasnikov <daniel.g.myasnikov@gmail.com>
2015-10-24 21:23:46 +1100

    [HOTFIX] login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-    redirect_to home_path_for(resource)
+    redirect_to home_path_for(current_admin)
   end
 
 protected
   def after_sign_in_path_for(resource)
-    home_path_for(resource)
+    home_path_for(current_admin)
+  end
+
+  def home_path_for(resource)
+    if resource.super_admin?
+      admin_companies_path
+    else
+      admin_products_path
+    end
   end
 
   def after_sign_out_path_for(resource_or_scope)

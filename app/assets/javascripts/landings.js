f0818f395fe54035a8253640bc65f849cb5c4f1e
Daniel Myasnikov <daniel.myasnikov@redant.com.au>
2016-03-16 09:57:14 +1100

    landing page redesign

--- app/assets/javascripts/landings.js
+++ app/assets/javascripts/landings.js
@@ -1,6 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
-//= require jquery
+
+//= require jquery-2.1.1.js
 //= require jquery_ujs
+//= require js.cookie
+//= require bootstrap.min.js
+//= require plugins/metisMenu/jquery.metisMenu.js
+//= require plugins/slimscroll/jquery.slimscroll.min.js
+//= require bootstrap-datetimepicker
+//= require inspinia.js
+//= require plugins/pace/pace.min.js
+
+
+// override your things in here
 //= require landings/app
